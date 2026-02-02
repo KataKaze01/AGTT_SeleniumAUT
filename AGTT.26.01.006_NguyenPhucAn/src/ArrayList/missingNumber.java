8c006e57c14d888150c640c06bef22c3265581d8
KataKaze <fanmu2122@gmail.com>
2026-02-02 17:14:37 +0700

    update 3

--- AGTT.26.01.006_NguyenPhucAn/src/ArrayList/missingNumber.java
+++ AGTT.26.01.006_NguyenPhucAn/src/ArrayList/missingNumber.java
@@ -5,24 +5,24 @@ import java.util.Scanner;
 public class missingNumber {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Enter n: ");
 		int n = sc.nextInt();
-		
+
 		int[] arr = new int[n - 1];
-		System.out.println("Enter array elements: ");
-		
-		for (int i = 0; i < n; i++) {
+
+		System.out.println("Enter array elements:");
+		for (int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
-		
+
 		int sumExpected = n * (n + 1) / 2;
 		int sumArray = 0;
-		
-		for(int num : arr) {
+
+		for (int num : arr) {
 			sumArray += num;
-		}
-		
-		System.out.println("Missing number is: " + (sumArray - sumArray));
+	}
+
+		System.out.println("Missing number is: " + (sumExpected - sumArray));
 	}
 }
